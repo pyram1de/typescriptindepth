d2474c967d1ecf628f9062af27c134ce665256f2
David Hulmes <David.Hulmes@airsweb.com>
2018-01-16 13:59:28 +0000

    initial commit

---
+++ src/app.ts
@@ -0,0 +1,27 @@
+function GetAllBooks() {
+    let books = [
+        {title: 'test1', author: 'test1', available: true},
+        {title: 'test2', author: 'test2', available: true},
+        {title: 'test3', author: 'test3', available: true},
+    ]
+
+    return books;
+}
+
+function LogFirstAvailable(books){
+    let numberOfBooks = books.length;
+
+    for (const currentBook of books) {
+        let firstAvailable = '';
+        if(currentBook.available){
+            firstAvailable = currentBook.title;
+            break;
+        }
+    }
+    console.log('total books: ' + numberOfBooks);
+    console.log('first available: ' + firstAvailable);
+}
+
+const allBooks = GetAllBooks();
+LogFirstAvailable(allBooks);
+
